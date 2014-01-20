d7143914968cfc437a5fc368a2521a11deff82e8
Nicki <nkeszler10@gmail.com>
2014-01-20 20:30:31 +0000

    Shadows added under logos. Pre logo labels

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,68 @@
 function shrinkNavbar(){
 	$('#navbar').addClass('shrink').removeClass('grow');
+	$('#nav_circle').addClass('shrink').removeClass('grow');
 	$('li').hide();
 };
 
 function growNavbar(){
 	$('#navbar').addClass('grow').removeClass('shrink');
-	$('li').show();
+	$('#nav_circle').addClass('grow').removeClass('shrink');
+	$('li').slideDown(400);
 };
+
+function toggleSignature(opacity){
+	if (opacity <= 0){
+		$('#signature').hide();
+		$('#signature_contact').hide();	
+	}else{
+		$('#signature').show();
+		$('#signature_contact').show();	
+	}
+}
+
+function fadeSignatureAndLogos(scrollIndex){
+	$('#signature').css({'opacity': 1 - (scrollIndex / 225) });
+	$('#signature_contact').css({'opacity': 1 - (scrollIndex / 225) });
+	$('.logos img').css({'opacity': Math.min(0.8, (scrollIndex - 200) / 250)});
+	// Hides and Shows Signature for overlap issue
+	toggleSignature($('#signature').css('opacity'));
+}
+
+function hoverIn(object, lastScroll){
+	if(lastScroll > 225){
+		object.addClass('hover');
+	}
+}
+
+function hoverOut(object, lastScroll){
+	object.removeClass('hover');
+}
 
 $(document).ready(function(){
 
-	var lastScroll = 0;
-	$(window).on('scroll', function(){
+	var lastScroll = $(window).scrollTop();
 
+	// $('.dragMe').draggable({axis: 'y'});
+
+	// Hover Logos
+	$('.logo-container').hover(function() { hoverIn($(this).children(),lastScroll) }, function() { hoverOut($(this).children(),lastScroll) });
+
+	// Detect Scroll - Shrink Navbar + Fade Logos/Signature + Adjust Header Img
+	$(window).on('scroll', function(){
 		var scrollIndex = $(window).scrollTop();
-		$('#header_img').css({'margin-top' :-170 + (scrollIndex*0.5)});
-		console.log(scrollIndex);
+		fadeSignatureAndLogos(scrollIndex);
+		// Moves header_img for perspective effect
+		$('#header_img').css({'margin-top' :-130 + (scrollIndex*0.5)});
+		
 		if (scrollIndex > 433){
 			shrinkNavbar();
-			$('#nav_circle').addClass('shrink').removeClass('grow');
 		}else{
 			growNavbar();
-			$('#nav_circle').addClass('grow').removeClass('shrink');
 		}
+		lastScroll = scrollIndex;
+	})
+
+});
 
 // For scroll up and down
   //   if (scrollIndex > lastScroll){
@@ -32,11 +72,6 @@ $(document).ready(function(){
   //      $('#header_img').css({'margin-top' :'-=2px'});
   //   }
   //   lastScroll = scrollIndex;
-})
-	
-
-});
-
 
 	// $('.moving').on('mousedown',function(event){
 	// 	offsetX = event.offsetX;
