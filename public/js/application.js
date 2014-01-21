08d1fdd34db8343dc1bc642c94f0ad688ccf3630
Nicki <nkeszler10@gmail.com>
2014-01-21 14:26:24 +0000

    Labels done + Logo bob and partial % instead of px

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
-function navbarToggle(scrollIndex){
-	if (scrollIndex > 433){
+function navbarToggle(scrollIndex, scrollAmount){
+	if (scrollIndex > (scrollAmount*0.85)){ // was 433
 		shrinkNavbar();
 	}else{
 		growNavbar();
@@ -24,48 +24,73 @@ function toggleSignature(opacity){
 		$('#signature_contact').show();	
 	}
 }
-function fadeSignatureAndLogos(scrollIndex){
-	$('#signature').css({'opacity': 1 - (scrollIndex / 225) });
-	$('#signature_contact').css({'opacity': 1 - (scrollIndex / 225) });
-	$('.logos img').css({'opacity': Math.min(0.8, (scrollIndex - 200) / 250)});
+function fadeSignatureAndLogos(scrollIndex, scrollAmount){
+	$('#signature').css({'opacity': 1 - (scrollIndex / (scrollAmount*0.44)) }); // was 225
+	$('#signature_contact').css({'opacity': 1 - (scrollIndex / (scrollAmount*0.44)) }); // was 225
+	$('.logos img').css({'opacity': Math.min(0.8, (scrollIndex - (scrollAmount*0.39)) / (scrollAmount*0.49))}); // was 200 / 250
 	// Hides and Shows Signature for overlap issue
 	toggleSignature($('#signature').css('opacity'));
+}
+function displayContactText(object){
+	$('.text-area *').hide();
+	$('.text-area').append('<h1 id="contact-text">'+object.children('.logo').attr('data-text')+'</h1>');
+}
+function removeContactText(){
+	$('.text-area *').show();
+	$('#contact-text').remove();
 }
 function hoverIn(object, lastScroll){
 	if(lastScroll > 225){
 		object.children().addClass('hover');
-		object.children().animate({width: "+=1px", height: "+=1px",'margin-left': "-=1px"},4);
-		// object.parent().children().not(object).children().animate({width: "-=1px", height: "-=1px"},4);
+		object.children().animate({width: "+=1px", height: "+=1px",'margin-left': "-=1px",'margin-bottom': "-=1px"},4);
+		displayContactText(object);
+		//$('.logo-container:not(.hover) img').animate({width: "-=1px", height: "-=1px"}, 4);  < --- Opposite Shrink/Grow
 	}
 }
 function hoverOut(object, lastScroll){
+		//$('.logo-container:not(.hover) img').animate({width: "+=1px", height: "+=1px"}, 4);  < --- Opposite Shrink/Grow
 		object.children().removeClass('hover');
-		object.children().animate({width: "-=1px", height: "-=1px",'margin-left': "+=1px"},4);
-		// object.parent().children().not(object).children().animate({width: "+=1px", height: "+=1px"},4);
+		object.children().animate({width: "-=1px", height: "-=1px",'margin-left': "+=1px",'margin-bottom': "+=1px"},4);
+		removeContactText();
+}
+function bob(){
+	$('.logos .logo').animate({'margin-top': '-=5px', 'margin-bottom': '+=5px'},800,function(){
+		$('.logos .logo').animate({'margin-top': '+=5px', 'margin-bottom': '-=5px'},800,bob());
+	});
+	// $('.logos img:not(.logo)').animate({'opacity':'-=0.1'},800,function(){
+	// 	$('.logos img:not(.logo)').animate({'opacity':'+=0.1'},800,bob());
+	// });
 }
 
 $(document).ready(function(){
 
+	var scrollAmount = $(document).height() - window.innerHeight;
+	console.log(scrollAmount);
 	var lastScroll = $(window).scrollTop();
 		// $('.dragMe').draggable({axis: 'y'});
 
 	// Detect Scroll - Shrink Navbar + Fade Logos/Signature + Adjust Header Img
 	$(window).on('scroll', function(){
-		// Disable hover
+		// Disable hover: Hover + Contact Text
 		$('.logo-container>img').removeClass('hover');
+		$('#contact-text').remove();
+		$('.text-area *').show()
 
 		var scrollIndex = $(window).scrollTop();
 		// Moves header_img for perspective effect
 		$('#header_img').css({'margin-top' :-130 + (scrollIndex*0.5)});
 
-		navbarToggle(scrollIndex);
-		fadeSignatureAndLogos(scrollIndex);
+		navbarToggle(scrollIndex, scrollAmount);
+		fadeSignatureAndLogos(scrollIndex, scrollAmount);
 
 		lastScroll = scrollIndex;
 	})
+
+	// bob($('.logos img:not(.logo)'));
+	bob();
 	
 	// Hover Logos
-	$('.logo-container').hover(function() { hoverIn($(this),lastScroll) }, function() { hoverOut($(this),lastScroll) });
+	$('.logo-container').hover(function() { hoverIn($(this),lastScroll) }, function() { hoverOut($(this),lastScroll)});
 
 });
 
